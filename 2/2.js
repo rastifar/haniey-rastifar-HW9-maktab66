14dc71b06ba6f4055f73293075d1f5fb51f964ad
haniey <h.rastifar@mail.sbu.ac.ir>
2022-01-04 13:13:48 -0800

    q2 changed

--- 2/2.js
+++ 2/2.js
@@ -41,43 +41,4 @@ stackCalc("5 8 + 4 5 1 + POP 13 + +");
 stackCalc("5 8  POP 13 +");
 stackCalc("x y +");
 
-// stackCalc("") ➞ 0
-// StackCalc("5 6 +") ➞ 11
-// StackCalc("3 DUP +") ➞ 6
-// StackCalc("6 5 5 7 * - /") ➞ 5
-//  StackCalc("x y +") ➞ Invalid instruction: x
-// else if(element === ""){
-//     result.push("0")
-//     return false
-// }
-// def run(instructions):
-//   lst = instructions.split()
-//   stack = []
 
-//   if not lst:
-//     return 0
-
-//   for i in lst:
-//     if i.isdigit():
-//       stack.append(int(i))
-//     elif i == "+":
-//       stack.append(stack.pop() + stack.pop())
-//     elif i == "-":
-//       stack.append(stack.pop() - stack.pop())
-//     elif i == "*":
-//       stack.append(stack.pop() * stack.pop())
-//     elif i == "/":
-//       stack.append(stack.pop() / stack.pop())
-//     elif i == "DUP":
-//       stack.append(stack[-1])
-//     elif i == "POP":
-//       stack.pop()
-//     else:
-//       return "Invalid instruction: " + i
-//   return stack.pop()
-
-// assert run('6 5 5 7 * - /') == 5
-// assert run('4 2 4 * 3 + 5 + / 5 -') == 1
-// assert run('5 8 + 4 5 1 + POP 13 +') == 17
-// assert run('x') == 'Invalid instruction: x'
-// assert run('') == 0
